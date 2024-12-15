8c3197a6cfa97aeb2e4c4c3df3b437a60ae6f5a9
capdavide278 <franc.davi00@gmail.com>
2024-12-15 20:36:36 +0100

    modifiche varie al css e js

--- script/script.js
+++ script/script.js
@@ -4,6 +4,9 @@ let avanti = true
 
 const cursore = document.getElementById('cursore-linea')
 const button = document.getElementById('cursore-linea')
+const vignetteContainer = document.getElementById('vignette-container');
+const testiVignette = Array.from(document.querySelectorAll('#testi-vignette span')); // Leggi i testi
+let testoCorrente = 0;
 
 
 function avanzaTempo(){
@@ -17,6 +20,22 @@ function avanzaTempo(){
         posizione -=96
         avanti = true
     }
-     cursore.style.left = `${posizione}%`
+    cursore.style.left = `${posizione}%`
+    cambiaColoreLinea();
 }
+
+function cambiaColoreLinea() {
+    const linea = document.getElementById('linea-tempo');
+  
+    // Cambia il colore gradualmente in base alla posizione
+    const percentuale = (posizione / Lunghezzalinea) * 100;
+    linea.style.background = `linear-gradient(to right, rgb(240, 114, 25) ${percentuale}%, #808080 ${percentuale}%)`;  
+}
+
 button.addEventListener('click', avanzaTempo)
+
+const navLinks = document.querySelectorAll('li a')
+const indicator = document.createElement('div')
+indicator.classList.add('indicator')
+document.querySelector('ul').appendChild('indicator')
+
